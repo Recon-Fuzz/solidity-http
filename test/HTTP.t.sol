7a64512fed1e2ab91776e19d8d5f285b6bf4b85d
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2026-01-12 12:03:16 -0300

    Apply CI formatter style

--- test/HTTP.t.sol
+++ test/HTTP.t.sol
@@ -20,10 +20,9 @@ contract HTTPTest is Test {
     }
 
     function test_HTTP_GET_options() public {
-        HTTP.Response memory res = http.initialize("https://httpbin.org/headers").GET()
-            .withHeader("accept", "application/json")
-            .withHeader("Authorization", "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==")
-            .request();
+        HTTP.Response memory res = http.initialize("https://httpbin.org/headers").GET().withHeader(
+            "accept", "application/json"
+        ).withHeader("Authorization", "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==").request();
 
         assertEq(res.status, 200);
 
@@ -86,10 +85,9 @@ contract HTTPTest is Test {
     }
 
     function test_HTTP_redirects_enabled() public {
-        HTTP.Response memory res = http.initialize().GET("https://httpbin.org/relative-redirect/1")
-            .withFollowRedirects(true)
-            .withMaxRedirects(3)
-            .request();
+        HTTP.Response memory res = http.initialize().GET("https://httpbin.org/relative-redirect/1").withFollowRedirects(
+            true
+        ).withMaxRedirects(3).request();
 
         assertEq(res.status, 200);
         assertTrue(res.data.toSlice().contains(("https://httpbin.org/get").toSlice()));
